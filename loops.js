f01f6e7f47526bc908743e1acbb7c3e2fbc8e1b3
Joe Tustin <j.tustin@gmail.com>
2019-02-17 01:00:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      return "I am 1 strange loop.";
+      console.log("I am 1 strange loop.");
     }
     else {
-      return `I am ${i} strange loops.`;
+      console.log(`I am ${i} strange loops.`);
     }
   }
 }
