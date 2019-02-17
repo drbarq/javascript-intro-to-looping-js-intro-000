950c53effb1db1f85661b18901850e2dccf4352f
Joe Tustin <j.tustin@gmail.com>
2019-02-17 02:57:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function incrementVariable() {
 }
 
 do {
-    console.log("I run once regardless.")
+    console.log("I run once regardless.");
   }
   while (incrementVariable() < num) {
     function doWhileLoop(num) {
