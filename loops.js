5bbec7b1b1db27a29a90b28d8a7cf0ad3cc6f122
Joe Tustin <j.tustin@gmail.com>
2019-02-17 02:40:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,3 +18,20 @@ function whileLoop(n) {
   }
   return "done";
 }
+
+function incrementVariable() {
+  i = i - 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+
+/*
+function doWhileLoop(num) {
+
+
+}
+
+*/
