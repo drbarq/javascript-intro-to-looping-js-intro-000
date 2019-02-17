e52455ada2bdf9bd464272bde63190f388b6782e
Joe Tustin <j.tustin@gmail.com>
2019-02-17 01:36:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@
 
 function forLoop(array) {
   var hashBrown = []
-  for (var i = 0; i < 25; i++) {
+  for (var i = 1; i < 25; i++) {
     if (i === 1) {
       hashBrown.push("I am 1 strange loop.");
     }
